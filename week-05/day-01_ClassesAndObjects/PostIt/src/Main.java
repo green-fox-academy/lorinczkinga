37d2b762c2d9b3f110bf6a096fad0f367340745f
lorinczkinga <lorincz.kinga1991@gmail.com>
2018-05-03 19:07:50 +0200

    added PostIt

---
+++ week-05/day-01_ClassesAndObjects/PostIt/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        PostIt postIt1;
+        postIt1 = new PostIt();
+        postIt1.backgroundColor = "orange";
+        postIt1.text = "Idea 1";
+        postIt1.textColor = "blue";
+
+        PostIt postIt2;
+        postIt2 = new PostIt();
+        postIt2.backgroundColor = "pink";
+        postIt2.text = "Awesome";
+        postIt2.textColor = "black";
+
+        PostIt postIt3;
+        postIt3 = new PostIt();
+        postIt3.backgroundColor = "yellow";
+        postIt3.text = "Superb";
+        postIt3.textColor = "green";
+
+    }
+}
