421e6e6941ae92a2665278b31ff1597f8ee2aa71
lorinczkinga <lorincz.kinga1991@gmail.com>
2018-05-05 12:39:23 +0200

    Postit reviewed

--- week-05/day-01_ClassesAndObjects/PostIt/src/Main.java
+++ week-05/day-01_ClassesAndObjects/PostIt/src/Main.java
@@ -1,23 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        PostIt postIt1;
-        postIt1 = new PostIt();
-        postIt1.backgroundColor = "orange";
-        postIt1.text = "Idea 1";
-        postIt1.textColor = "blue";
+        PostIt postIt1 = new PostIt("orange", "Idea 1", "blue");
+        System.out.println(postIt1.text);
 
-        PostIt postIt2;
-        postIt2 = new PostIt();
-        postIt2.backgroundColor = "pink";
-        postIt2.text = "Awesome";
-        postIt2.textColor = "black";
+        PostIt postIt2 = new PostIt("pink", "Awesome", "black");
+        System.out.println(postIt2.text);
 
-        PostIt postIt3;
-        postIt3 = new PostIt();
-        postIt3.backgroundColor = "yellow";
-        postIt3.text = "Superb";
-        postIt3.textColor = "green";
-
+        PostIt postIt3 = new PostIt("yellow", "Superb", "green");
+        System.out.println(postIt3.text);
     }
 }
--- week-05/day-01_ClassesAndObjects/PostIt/src/PostIt.java
+++ week-05/day-01_ClassesAndObjects/PostIt/src/PostIt.java
@@ -2,4 +2,10 @@ public class PostIt {
     String backgroundColor;
     String text;
     String textColor;
+
+    public PostIt(String backgroundColor, String text, String textColor) {
+        this.backgroundColor = backgroundColor;
+        this.text = text;
+        this.textColor = textColor;
+    }
 }
