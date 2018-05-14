b29dc8e09af121efe9673a4aca618ca1dcfc4653
lorinczkinga <lorincz.kinga1991@gmail.com>
2018-05-14 18:45:08 +0200

    Added Tree

---
+++ week-05/day-01_ClassesAndObjects/Tree/src/Tree.java
@@ -0,0 +1,6 @@
+public class Tree {
+    private String type;
+    private String leafColor;
+    private int age;
+    private String sex;
+}
---
+++ week-05/day-01_ClassesAndObjects/Tree/src/TreeMain.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TreeMain {
+    public static void main(String[] args) {
+        List<Tree> treeList = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            treeList.add(new Tree());
+        }
+    }
+}
