e7816ee58d9cd53e95db98dde6b62e54398a8efc
lorinczkinga <lorincz.kinga1991@gmail.com>
2018-05-18 05:34:02 +0200

    Modified CountLines

--- week-04/day-01/src/CountLines.java
+++ week-04/day-01/src/CountLines.java
@@ -5,19 +5,20 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class CountLines {
+    
     public static void main(String[] args) {
         String filename = "my-file.txt";
         System.out.println(countLines(filename));
-
     }
-        public static int countLines(String filename) {
 
-            try {
-                Path path = Paths.get(filename);
-                List<String> lines = Files.readAllLines(path);
-                return lines.size();
-            } catch (IOException e) {
-                return 0;
-            }
+    public static int countLines(String filename) {
+
+        try {
+            Path path = Paths.get(filename);
+            List<String> lines = Files.readAllLines(path);
+            return lines.size();
+        } catch (IOException e) {
+            return 0;
         }
+    }
 }
