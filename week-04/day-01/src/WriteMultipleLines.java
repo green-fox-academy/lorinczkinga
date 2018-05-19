7bdaf4a6e61a9958c7ff8d6fc47214c2c49031da
lorinczkinga <lorincz.kinga1991@gmail.com>
2018-05-19 12:48:07 +0200

    Added WriteMultipleLines

---
+++ week-04/day-01/src/WriteMultipleLines.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WriteMultipleLines {
+    public static void main(String[] args) {
+
+        WriteMultipleLines("my-file3.txt", "WriteMultipleLineTask", 10);
+
+    }
+
+    public static void WriteMultipleLines(String path, String lineContent, int numberOfLines) {
+        List<String> myContent = new ArrayList<>();
+        for (int i = 0; i < numberOfLines; i++) {
+            myContent.add(lineContent);
+        }
+        Path pathVariable = Paths.get(path);
+        try {
+            Files.write(pathVariable, myContent);
+            //for (int i = 0; i < numberOfLines; i++) {
+            //    System.out.println(Files.readAllLines(pathVariable).get(i));
+            //}
+        } catch (IOException e) {
+            System.out.println("Unable to write file: my-file3.txt");
+        }
+    }
+}
