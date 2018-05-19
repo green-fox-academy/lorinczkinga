144885ea880667fb6823ff93c41c65f0da7a1df1
lorinczkinga <lorincz.kinga1991@gmail.com>
2018-05-19 23:19:52 +0200

    Adding ReversedOrder.java

--- week-04/day-01_FileManipulation/src/ReversedOrder.java
+++ week-04/day-01_FileManipulation/src/ReversedOrder.java
@@ -5,25 +5,21 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ReversedLines {
+public class ReversedOrder {
 
     public static void main(String[] args) {
-        decryptReversedLines("reversed-lines.txt");
+        decryptReversedOrder("reversed-order.txt");
     }
 
-    private static List<String> decryptReversedLines(String filePath) {
+    private static List<String> decryptReversedOrder(String filePath) {
         Path path = Paths.get(filePath);
         List<String> decryptedText = new ArrayList<>();
         try {
             List<String> originalText = new ArrayList<>(Files.readAllLines(path));
 
-            for (int i = 0; i < originalText.size(); i++) {
-                decryptedText.add(i, "");
-                for (int j = 1; j < originalText.get(i).length()+1; j++) {
-                    decryptedText.set(i, decryptedText.get(i)
-                            .concat("" + originalText.get(i).charAt(originalText.get(i).length() - j)));
-                }
-                System.out.println(decryptedText.get(i));
+            for (int i = 1; i < originalText.size()+1 ; i++) {
+                decryptedText.add(i-1, originalText.get(originalText.size()-i));
+                System.out.println(decryptedText.get(i-1));
             }
             return decryptedText;
         } catch (IOException e) {
