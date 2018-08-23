f7a4580a746c968eba797287cfd48ba2d3969bcf
lorinczkinga <lorincz.kinga1991@gmail.com>
2018-08-23 20:35:30 +0200

    refactor(AnagramTest.java)

--- week-07/day-01_Testing/Anagram/Test/AnagramTest.java
+++ week-07/day-01_Testing/Anagram/Test/AnagramTest.java
@@ -1,30 +1,34 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class AnagramTest {
 
+    private Anagram myAnagram;
+
+    @Before
+    public void init() {
+        myAnagram = new Anagram();
+    }
+
     @Test
     public void testAnagramFunctionWithEmptyStrings() {
-        Anagram myAnagram = new Anagram();
-        assertEquals(true, myAnagram.anagramFunction("",""));
+        assertTrue(myAnagram.anagramFunction("", ""));
     }
 
     @Test
     public void testAnagramFunctionWithAnagram() {
-        Anagram myAnagram = new Anagram();
-        assertEquals(true, myAnagram.anagramFunction("aba aba","aba aba"));
+        assertTrue(myAnagram.anagramFunction("aba aba", "aba aba"));
     }
 
     @Test
     public void testAnagramFunctionWithNonAnagram() {
-        Anagram myAnagram = new Anagram();
-        assertEquals(false, myAnagram.anagramFunction("baaba","caaba"));
+        assertFalse(myAnagram.anagramFunction("baaba", "caaba"));
     }
 
     @Test
     public void testAnagramFunctionWithUpperCase() {
-        Anagram myAnagram = new Anagram();
-        assertEquals(true, myAnagram.anagramFunction("Aaba","abaA"));
+        assertTrue(myAnagram.anagramFunction("Aaba", "abaA"));
     }
 }
